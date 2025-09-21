2c798c94b771cc4a683661309c210bbf65111421
ankitjnvynr <ankitbkana@outlook.com>
2025-09-21 09:50:35 +0530

    added route for current user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, getAllUsers, googleLoginUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getAllUsers, getCurrentUser, googleLoginUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -20,6 +20,7 @@ router.route("/google").post(googleLoginUser)
 router.route("/update-password").post(verifyJWT,changeCurrentPassword)
 
 //secure routes
+router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-profile").post(verifyJWT,updateAccountDetails)
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/all").get(verifyJWT,getAllUsers)
